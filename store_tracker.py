9cd73d97b4f9813b5531931ca991b3bca3fbf33b
bryan <bryan04c@gmail.com>
2025-01-10 21:23:56 -0500

    finished the track_amazon() function in the store_tracker.py file

--- store_tracker.py
+++ store_tracker.py
@@ -20,15 +20,32 @@ def track_best_buy(item_url) -> float:
 
     return price
 
-# x = track_best_buy('https://www.bestbuy.com/site/razer-blade-16-16-gaming-laptop-dual-mini-led-4k-uhd-fhd-intel-i9-hx-nvidia-geforce-rtx-4080-32gb-ram-2tb-ssd-mercury/6570244.p?skuId=6570244')
+
+# x = track_best_buy('https://www.bestbuy.com/site/razer-blade-16-16-gaming-laptop-dual-mini-led-4k-uhd-fhd-intel-i9'
+#                    '-hx-nvidia-geforce-rtx-4080-32gb-ram-2tb-ssd-mercury/6570244.p?skuId=6570244')
 # print(x)
 
 
 def track_amazon(item_url) -> float:
+    driver = webdriver.Firefox()
+    driver.get(item_url)
+    time.sleep(0.5)
 
-    price = 0
+    price_element = driver.find_element(By.CLASS_NAME, 'a-price-whole')
+    price = float(price_element.text)
+    price_cent_element = driver.find_element(By.CLASS_NAME, 'a-price-fraction')
+    price += float(price_cent_element.text) * 0.01
+    driver.quit()
 
     return price
+
+
+# y = track_amazon('https://www.amazon.com/Racing-Gaming-Simulator-Bundle-Steering-PC/dp/B07DW25P3R/ref=pd_ci'
+#                  '_mcx_mh_mcx_views_0_image?pd_rd_w=xWTd6&content-id=amzn1.sym.bb21fc54-1dd8-448e-92bb-2ddce18'
+#                  '7f4ac%3Aamzn1.symc.40e6a10e-cbc4-4fa5-81e3-4435ff64d03b&pf_rd_p=bb21fc54-1dd8-448e-92bb-2ddce'
+#                  '187f4ac&pf_rd_r=4NRGP0335XRVF31DS8ZQ&pd_rd_wg=Pambw&pd_rd_r=e2331406-455b-4c44-9472-b10c8e1'
+#                  'bab54&pd_rd_i=B07DW25P3R&th=1')
+# print(y)
 
 
 # not all products are available on one or both websites
