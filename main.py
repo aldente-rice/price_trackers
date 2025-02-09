cf87717e50748394f4c3be1ae5bc73ffd391da52
bryan <bryan04c@gmail.com>
2025-02-09 14:01:35 -0500

    added a new file 'price_data.csv' that will track the prices of the items (across amazon and bestbuy)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import pandas
+import pandas as pd
 import matplotlib
 from store_tracker import track_best_buy, track_amazon
 
+df = pd.read_csv('price_data.csv')
 
 def track_price():
     return None
