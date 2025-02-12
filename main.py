e3eaecef8b0df4b91a613d27b83926b52cfece43
bryan <bryan04c@gmail.com>
2025-02-12 13:24:25 -0500

    need to fix the amazon captcha solver

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import pandas as pd
 import matplotlib
-from store_tracker import track_best_buy, track_amazon
+from store_tracker import track_best_buy, track_amazon, bestbuy_to_amazon, amazon_to_bestbuy
 
+
+pd.set_option('display.max_columns', None) # displays all columns
+pd.set_option('display.width', 100) # max number of chars displayed in a line (for .head())
 df = pd.read_csv('price_data.csv')
+print(df.head())
 
-def track_price():
+# gathers information (price, URLs, date modified)
+temp_link_bb = 'https://www.bestbuy.com/site/apple-airpods-4-white/6447384.p?skuId=6447384'
+price_1 = track_best_buy(temp_link_bb)
+temp_link_am = bestbuy_to_amazon(temp_link_bb)
+price_2 = track_amazon(temp_link_am)
+
+print('BestBuy: ', price_1, '\nAmazon: ', track_amazon(price_2))
+
+# print(df.head())
+
+
+# @parameters
+# item_url: the item url of where the users retrieved the item
+# store: the website the user retrieved the item from (Amazon or Bestbuy)
+#
+# price track interval: 1 hour
+#
+# keeps track of an item from either Amazon or Bestbuy
+# stores the data to then visualize data
+def track_price(item_url, store):
     return None
 
 
